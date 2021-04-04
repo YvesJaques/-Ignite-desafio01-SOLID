6a762a657042fd8dd50a282ee6e435acb9eecedd
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-04-04 14:47:41 -0300

    Implementacao de estrutura SOLID

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,14 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = <{ user_id: string }>request.headers;
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+      return response.status(200).send(users);
+    } catch (error) {
+      return response.status(400).send({ error: error.message });
+    }
   }
 }
 
